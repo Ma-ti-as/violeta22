3045ca1e327162193b868207a419348d18e11652
Ma-ti-as <saitamsajor.com>
2022-12-14 20:04:35 +0100

    Ánimo Joe

--- codi/Exercicis/factorial.js
+++ codi/Exercicis/factorial.js
@@ -1,23 +1,31 @@
 
 
 num=Number(prompt('Dame un número:'));
-
 factorial=1;
 
 // For loop
-for(i=num; i>0; i--){
+
+/* for(i=num; i>0; i--){
     factorial=factorial*i;
 }
-document.write(factorial);
+document.write(factorial); */
 
 
 
 // While loop
 
-num2 = Number(prompt('Dame un número:'));
-factorial = 1
-
-while (num2 > 1){
+/* while (num2 > 1){
     factorial = factorial * num2
     num2 = num2 - 1;}
-    document.write(factorial);
+    document.write(factorial); */
+
+
+    
+//Do while loop
+
+do{
+    factorial = factorial * num
+    num = num - 1;
+}
+while(num>1);document.write(factorial);
+
